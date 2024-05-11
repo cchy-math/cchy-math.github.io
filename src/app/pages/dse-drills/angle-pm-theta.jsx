9a14623c74e2b02f39a4bae41c99617879ab065a
cchy <cchy@tstss.edu.hk>
2024-05-11 15:28:19 +0800

    Answer visibility done

--- src/app/pages/dse-drills/angle-pm-theta.jsx
+++ src/app/pages/dse-drills/angle-pm-theta.jsx
@@ -14,7 +14,13 @@ export default function AnglePmAngle() {
 			window.MathJax.typesetClear()
 			window.MathJax.typeset()
 		}
-	},[texQ])
+	},[texQ]);
+    useEffect(()=>{
+		if( typeof window?.MathJax !== "undefined"){
+			window.MathJax.typesetClear()
+			window.MathJax.typeset()
+		}
+	},[texA_eng]);
     useEffect(()=>{
 		if( typeof window?.MathJax !== "undefined"){
 			window.MathJax.typesetClear()
@@ -37,16 +43,16 @@ export default function AnglePmAngle() {
     return (
         
 		<main className="px-4 py-3">
-			<div className="display-5">
+			<div className="h1">
                 Let {"\\( \\ \\theta \\ \\)"} be an acute angle. Determine which quadrant does the folowing angle lies on.
-                <br/>
+                <br/><br/>
                 設 {"\\( \\ \\theta \\ \\)"} 為銳角，判斷下列角度所屬的象限。
                 <br/><br/><br/><br/>
                 <h1 className="display-1 d-flex justify-content-center">
                     {texQ}
                 </h1>
                 <br/><br/><br/><br/>
-                <div class="row my-5">
+                <div class="row mb-5">
                     <div class="col-1 col-sm-2 d-flex justify-content-center"></div>
                     <div class="col-1 col-sm-2 d-flex justify-content-center"></div>
                     <div class="col-4 col-sm-2 d-flex justify-content-center">
@@ -63,13 +69,22 @@ export default function AnglePmAngle() {
                             setTexQ(eq1+eq2+eq3+eq4+eq5);
                             let ans = (Math.floor(Number(eq2)/90) + pm) % 4;
                             if(ans == 0){
-                                setTexA_eng("Quadrant IV");
-                                setTexA_chi("象限 IV");
-                            }else{
-                                setTexA_eng(ans);
-                                setTexA_chi(ans);
+                                setTexA_eng("\\( \\text{Quadrant IV} \\)");
+                                setTexA_chi("\\( \\text{象限 IV} \\)");
                             };
-                            setIsVisible(false)
+                            if(ans == 1){
+                                setTexA_eng("\\( \\text{Quadrant I} \\)");
+                                setTexA_chi("\\( \\text{象限 I} \\)");
+                            };
+                            if(ans == 2){
+                                setTexA_eng("\\( \\text{Quadrant II} \\)");
+                                setTexA_chi("\\( \\text{象限 II} \\)");
+                            };
+                            if(ans == 3){
+                                setTexA_eng("\\( \\text{Quadrant III} \\)");
+                                setTexA_chi("\\( \\text{象限 III} \\)");
+                            };
+                            setIsVisible(false);
                         }}>
                             Next
                         </button>
@@ -82,14 +97,21 @@ export default function AnglePmAngle() {
                     <div class="col-1 col-sm-2 d-flex justify-content-center"></div>
                     <div class="col-1 col-sm-2 d-flex justify-content-center"></div>
                 </div>
-                <div class="row">
-                    <div class="col-3 d-flex justify-content-center"></div>
-                    <div class={"col-6 d-flex justify-content-center" + isVisible ? 'visible' : 'invisible'}>
-                        {texA_eng}
-                        <br/>
-                        {texA_chi}
+                <div class="row mb-5">
+                    <div class="col-1 col-sm-2 d-flex justify-content-center"></div>
+                    <div class="col-1 col-sm-2 d-flex justify-content-center"></div>
+                    <div class="col-4 col-sm-2 d-flex justify-content-center">
+                        <div className={isVisible ? 'visible' : 'invisible'}>
+                            {texA_eng}
+                        </div>
                     </div>
-                    <div class="col-3 d-flex justify-content-center"></div>
+                    <div class="col-4 col-sm-2 d-flex justify-content-center">
+                        <div className={isVisible ? 'visible' : 'invisible'}>
+                            {texA_chi}
+                        </div>
+                    </div>
+                    <div class="col-1 col-sm-2 d-flex justify-content-center"></div>
+                    <div class="col-1 col-sm-2 d-flex justify-content-center"></div>
                 </div>
             </div>
 		</main>
