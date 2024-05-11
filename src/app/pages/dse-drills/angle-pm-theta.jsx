91e309a5eec375098077cd99b9299a34c44de3b1
cchy <cchy@tstss.edu.hk>
2024-05-11 14:46:33 +0800

    ASTC done

--- src/app/pages/dse-drills/angle-pm-theta.jsx
+++ src/app/pages/dse-drills/angle-pm-theta.jsx
@@ -6,37 +6,92 @@ function getRndInteger(min, max) {
 
 export default function AnglePmAngle() {
     
-	const [tex, setTex] =useState('');
+	const [texQ, setTexQ] =useState('');
+    const [texA_chi, setTexA_chi] =useState('');
+    const [texA_eng, setTexA_eng] =useState('');
     useEffect(()=>{
 		if( typeof window?.MathJax !== "undefined"){
 			window.MathJax.typesetClear()
 			window.MathJax.typeset()
 		}
-	},[tex])
-
+	},[texQ])
+    useEffect(()=>{
+		if( typeof window?.MathJax !== "undefined"){
+			window.MathJax.typesetClear()
+			window.MathJax.typeset()
+		}
+	},[texA_chi])
     let eq1 = "\\( \\ ";
     let eq2 = 90*getRndInteger(1,5);
-	let eq3 = "^{\\circ} ";
+    let eq3 = "^{\\circ} ";
     let eq4 = "-";
     let eq5 = " \\theta \\ \\)";
-    if(getRndInteger(1,3) == 1){
-        eq4 = "+"
-    }else{
-        eq4 = "-"
-    }
+    let i = 0;
+    let pm = 0;
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible);
+    };
     return (
         
-		<main className="px-4 py-3 col">
-			<div className="h-25 display-5">
+		<main className="px-4 py-3">
+			<div className="display-5">
                 Let {"\\( \\ \\theta \\ \\)"} be an acute angle. Determine which quadrant does the folowing angle lies on.
-            </div>
-            <div className="h-100 display-5">
+                <br/>
                 設 {"\\( \\ \\theta \\ \\)"} 為銳角，判斷下列角度所屬的象限。
+                <br/><br/><br/><br/>
+                <h1 className="display-1 d-flex justify-content-center">
+                    {texQ}
+                </h1>
+                <br/><br/><br/><br/>
+                <div class="row my-5">
+                    <div class="col-1 col-sm-2 d-flex justify-content-center"></div>
+                    <div class="col-1 col-sm-2 d-flex justify-content-center"></div>
+                    <div class="col-4 col-sm-2 d-flex justify-content-center">
+                        <button className="button-rainbow h4 w-75" onClick={() => {
+                            eq2 = 90*getRndInteger(1,5);
+                            i = getRndInteger(1,3);
+                            if(i == 2){
+                                eq4 = "+";
+                                pm = 1;
+                            }else{
+                                eq4 = "-";
+                                pm = 0;
+                            }
+                            setTexQ(eq1+eq2+eq3+eq4+eq5);
+                            let ans = (Math.floor(Number(eq2)/90) + pm) % 4;
+                            if(ans == 0){
+                                setTexA_eng("Quadrant IV");
+                                setTexA_chi("象限 IV");
+                            }else{
+                                setTexA_eng(ans);
+                                setTexA_chi(ans);
+                            };
+                            setIsVisible(false)
+                        }}>
+                            Next
+                        </button>
+                    </div>
+                    <div class="col-4 col-sm-2 d-flex justify-content-center">
+                        <button className="button-rainbow h4 w-75" onClick={toggleVisibility}>
+                            Answer
+                        </button>
+                    </div>
+                    <div class="col-1 col-sm-2 d-flex justify-content-center"></div>
+                    <div class="col-1 col-sm-2 d-flex justify-content-center"></div>
+                </div>
+                <div class="row">
+                    <div class="col-3 d-flex justify-content-center"></div>
+                    <div class={"col-6 d-flex justify-content-center" + isVisible ? 'visible' : 'invisible'}>
+                        {texA_eng}
+                        <br/>
+                        {texA_chi}
+                    </div>
+                    <div class="col-3 d-flex justify-content-center"></div>
+                </div>
             </div>
-            <div className="display-1 text-center w-100">
-                {eq1+eq2+eq3+eq4+eq5}
-            </div>
-            
 		</main>
 	);
 }
