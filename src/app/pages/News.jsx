b7bc861c9057fe89d0bafc6be0f437bc94b69958
cchy <cchy@tstss.edu.hk>
2024-05-14 19:04:27 +0800

    -

--- src/app/pages/News.jsx
+++ src/app/pages/News.jsx
@@ -47,6 +47,12 @@ export default function News() {
 			<div className="row align-items-center flex-nowrap h2 ps-3">
 				3B
 				<div className="row ms-3 flex-nowrap overflow-auto scroll-news">
+					<NewsCard 
+						title={'TSA 補底'}
+						color={''}
+						date={'2024 May 14'}
+						content={'TSA 補底將於5月20日星期一進行，同學請留意日期時間依時出席。'}
+					/>
 					<NewsCard
 						title={'Chapter Test 10'}
 						color={'success'}
