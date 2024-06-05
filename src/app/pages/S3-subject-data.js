8dfbc0114d1294788f50905689bc60e515797171
cchy <cchy@tstss.edu.hk>
2024-06-05 14:28:06 +0800

    -

--- src/app/pages/S3-subject-data.js
+++ src/app/pages/S3-subject-data.js
@@ -1,62 +1,110 @@
 export const S3subjectData = [
-    {
-      "subject": "CHI",
-      "chi_name": "中國語文", 
-      "eng_name": "Chinese Language"
-    },
-    {
-      "subject": "ENG",
-      "chi_name": "英國語文",
-      "eng_name": "English Language"
-    },
-    {
-      "subject": "MATH",
-      "chi_name": "數學",
-      "eng_name": "Mathematics"
-    },
-    {
-      "subject": "PHY",
-      "chi_name": "物理", 
-      "eng_name": "Physics"
-    },
-    {
-      "subject": "CHEM",
-      "chi_name": "化學",
-      "eng_name": "Chemistry"
-    }, 
-    {  
-      "subject": "BIO",
-      "chi_name": "生物",
-      "eng_name": "Biology"
-    },
-    {
-      "subject": "ECON",
-      "chi_name": "經濟",
-      "eng_name": "Economics"
-    },
-    {
-      "subject": "BUS",
-      "chi_name": "企業概論", 
-      "eng_name": "Business Studies"
-    },
-    {
-      "subject": "GEO", 
-      "chi_name": "地理",
-      "eng_name": "Geography"
-    },
-    {
-      "subject": "CHIS",
-      "chi_name": "中國歷史", 
-      "eng_name": "Chinese History"
-    },
-    {
-      "subject": "HIST",
-      "chi_name": "歷史",
-      "eng_name": "History"
-    },
-    {
-     "subject": "COM",
-     "chi_name": "電腦", 
-     "eng_name": "Computer Literacy"
-    }
-  ];
+  {
+    "subject": "CHI",
+    "chi_name": "中國語文", 
+    "eng_name": "Chinese Language",
+    "papers": [
+      {
+        "chi_name": "試卷（一） 閱讀", 
+        "eng_name": "Paper I Reading",
+      },
+      {
+        "chi_name": "試卷（二） 寫作", 
+        "eng_name": "Paper II Writing",
+      },
+      {
+        "chi_name": "試卷（三） 聆聽", 
+        "eng_name": "Paper III Listening",
+      },
+      
+      {
+        "chi_name": "試卷（四） 說話", 
+        "eng_name": "Paper IV Speaking",
+      },
+    ],
+  },
+  {
+    "subject": "ENG",
+    "chi_name": "英國語文",
+    "eng_name": "English Language",
+    "papers": [
+      {
+        "chi_name": "試卷（一） 閱讀", 
+        "eng_name": "Paper I Reading",
+      },
+      {
+        "chi_name": "試卷（二） 寫作", 
+        "eng_name": "Paper II Writing",
+      },
+      {
+        "chi_name": "試卷（三） 聆聽", 
+        "eng_name": "Paper III Listening",
+      },
+      
+      {
+        "chi_name": "試卷（四） 說話", 
+        "eng_name": "Paper IV Speaking",
+      },
+    ],
+  },
+  {
+    "subject": "MATH",
+    "chi_name": "數學",
+    "eng_name": "Mathematics",
+    "papers": [
+      {
+        "chi_name": "試卷（一）", 
+        "eng_name": "Paper I",
+      },
+      {
+        "chi_name": "試卷（二）", 
+        "eng_name": "Paper II",
+      },
+    ],
+  },
+  {
+    "subject": "PHY",
+    "chi_name": "物理", 
+    "eng_name": "Physics"
+  },
+  {
+    "subject": "CHEM",
+    "chi_name": "化學",
+    "eng_name": "Chemistry"
+  }, 
+  {  
+    "subject": "BIO",
+    "chi_name": "生物",
+    "eng_name": "Biology"
+  },
+  {
+    "subject": "ECON",
+    "chi_name": "經濟",
+    "eng_name": "Economics"
+  },
+  {
+    "subject": "BUS",
+    "chi_name": "企業概論", 
+    "eng_name": "Business Studies"
+  },
+  {
+    "subject": "GEO", 
+    "chi_name": "地理",
+    "eng_name": "Geography"
+  },
+  {
+    "subject": "CHIS",
+    "chi_name": "中國歷史", 
+    "eng_name": "Chinese History"
+  },
+  {
+    "subject": "HIST",
+    "chi_name": "歷史",
+    "eng_name": "History"
+  },
+  {
+    "subject": "COM",
+    "chi_name": "電腦", 
+    "eng_name": "Computer Literacy"
+  }
+];
