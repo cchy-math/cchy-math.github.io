52e6addd0ebc06b8f59a03c3a7ef4797e533ab32
cchy <cchy@tstss.edu.hk>
2024-06-06 08:34:59 +0800

    -

--- src/app/pages/Timer.jsx
+++ src/app/pages/Timer.jsx
@@ -135,26 +135,26 @@ export default function Timer() {
                     </div>
                 </div>
             </section>
-            <section>
+            <section className="p-3">
                 <div className="row p-3">
-                    <div className="col-3 col-xs-12 p-3">
+                    <div className="col-4 col-xs-12 p-3">
                         <label for="display" class="form-label h4">Display</label>
-                        <select class="form-select bg-dark text-light" id="display" required="" value={form} onChange={e => setForm(e.target.value)}>
+                        <select class="form-select bg-dark text-light" id="display" required="">
                             <option value="CHI">中文 Chinese</option>
                             <option value="ENG">英文 English</option>
                             <option value="BOTH">雙語切換 Switch</option>
                         </select>
                     </div>
-                    <div className="col-5 col-xs-12 p-3">
+                    <div className="col-4 col-xs-12 p-3">
                         <label for="remaining-time" class="form-label h4">Remaining Time</label>
-                        <select class="form-select bg-dark text-light" id="remaining-time" required="" value={form} onChange={e => setForm(e.target.value)}>
+                        <select class="form-select bg-dark text-light" id="remaining-time" required="">
                             <option value="NO">沒有剩餘時間提示 No Remaining Time Reminder</option>
-                            <option value="15+5">時間剩餘15分鐘及5分鐘時提示 Remind when 15 minutes left and 5 minutes left</option>
+                            <option value="YES">時間剩餘15分鐘及5分鐘時提示 Remind when 15 minutes left and 5 minutes left</option>
                         </select>
                     </div>
                     <div className="col-4 col-xs-12 p-3">
                         <label for="auto" class="form-label h4">Auto Start</label>
-                        <select class="form-select bg-dark text-light" id="auto" required="" value={form} onChange={e => setForm(e.target.value)}>
+                        <select class="form-select bg-dark text-light" id="auto" required="">
                             <option value="MANUAL">手動開始 Start Manually</option>
                             <option value="AUTO">自動開始 Start Automatically</option>
                         </select>
