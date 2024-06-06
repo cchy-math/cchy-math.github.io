d6e0878a983504a80b4a8c1437fb538ac20f2986
cchy <cchy@tstss.edu.hk>
2024-06-06 08:03:00 +0800

    -

--- src/app/pages/Timer.jsx
+++ src/app/pages/Timer.jsx
@@ -112,24 +112,24 @@ export default function Timer() {
                         <label for="duration" class="form-label h4">Duration</label>
                         <div className="row">
                             <select class="col mx-3 form-select bg-dark text-light" id="duration-hour" required="">
-                                <option value="0">0</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
+                                <option value="0">0 hour</option>
+                                <option value="1">1 hour</option>
+                                <option value="2">2 hour</option>
+                                <option value="3">3 hour</option>
+                                <option value="4">4 hour</option>
                             </select>
                             <select class="col mx-3 form-select bg-dark text-light" id="duration-min" required="">
-                                <option value="5">05</option>
-                                <option value="10">10</option>
-                                <option value="15">15</option>
-                                <option value="20">20</option>
-                                <option value="25">25</option>
-                                <option value="30">30</option>
-                                <option value="35">35</option>
-                                <option value="40">40</option>
-                                <option value="45">45</option>
-                                <option value="50">50</option>
-                                <option value="55">55</option>
+                                <option value="5">5 min</option>
+                                <option value="10">10 min</option>
+                                <option value="15">15 min</option>
+                                <option value="20">20 min</option>
+                                <option value="25">25 min</option>
+                                <option value="30">30 min</option>
+                                <option value="35">35 min</option>
+                                <option value="40">40 min</option>
+                                <option value="45">45 min</option>
+                                <option value="50">50 min</option>
+                                <option value="55">55 min</option>
                                 
                             </select>
                         </div>
