bd80579782148f635753a5a723f236b30917d7fe
cchy <cchy@tstss.edu.hk>
2024-10-24 11:44:15 +0800

    -

--- src/app/HeaderBar.jsx
+++ src/app/HeaderBar.jsx
@@ -15,17 +15,25 @@ export default function HeaderBar() {
 		}
 	},[tex])
   return (
-    <header className="px-4 vww-100">
-      <div className="d-flex align-items-center justify-content-between pt-4" style={{ height: 10 }}>
-        <NavLink to="/menu"><button className="button-rainbow">
-          Menu
-        </button></NavLink>
-        <NavLink to="/" className="h2 text-white">
-          CCHY
-        </NavLink>
-        <button className="button-dark invisible">
-          Empty Space
-        </button>
+    <header className="px-4 vw-100">
+      <div className="row vw-100 px-4" style={{ height: 10 }}>
+        <div className="col-4 py-2">
+          <NavLink to="/menu">
+            <button className="button-rainbow">
+              Menu
+            </button>
+          </NavLink>
+        </div>
+        <div className="col-4">
+          <NavLink to="/" className="h1 text-white">
+            CCHY
+          </NavLink>
+        </div>
+        <div className="col-4">
+          <button className="button-dark invisible">
+            Empty Space
+          </button>
+        </div>
       </div>
       <div className="progress p-0">
         <div className="progress-bar progress-bar-striped progress-bar-animated" />
